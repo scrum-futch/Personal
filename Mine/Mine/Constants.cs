a417701619007abd8195a116059726cbcf4687ce
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-03 22:28:15 -0800

    Fixed Namespace for Constants.cs and TaskExtension.cs

--- Mine/Mine/Constants.cs
+++ Mine/Mine/Constants.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 
-namespace Game
+namespace Mine
 {
     public static class Constants
     {
--- Mine/Mine/TaskExtension.cs
+++ Mine/Mine/TaskExtension.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading.Tasks;
 
-namespace Game
+namespace Mine
 {
     public static class TaskExtensions
     {
