07dac2f49bc2e21cff02d7eb502303d8c3288c69
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-03 22:48:37 -0800

    Added Delete to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -74,10 +74,25 @@ namespace Mine.Services
             }
             return true;
         }
-
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Deletes items from the Database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            var data = await ReadAsync(id);
+            if (data == null)
+            {
+                return false;
+            }
+
+            var result = await Database.DeleteAsync(data);
+            if (result == 0)
+            {
+                return false;
+            }
+            return true;
         }
         /// <summary>
         /// Brings items to the readpage from the database
