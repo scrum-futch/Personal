b4b153b613c8d1fa9065fe41f49b12f84d7ee7fb
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-10 19:11:09 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            Assert.Pass();
+        }
+    }
+}
