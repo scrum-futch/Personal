597e905a9cbfdf1d32097c3910adbbc5b95d99a7
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-10 19:47:39 -0800

    Added RollDice Invalid Roll 1 Dice 0 Should Return zero to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -55,6 +55,15 @@ namespace UnitTests.Helpers {
             // Assert             
             Assert.AreEqual(0, result);
         }
+        public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+            // Reset
+            // Assert             
+            Assert.AreEqual(0, result);
+        }
     }
 
 }
