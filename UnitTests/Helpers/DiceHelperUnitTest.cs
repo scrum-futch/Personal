f251d6684f585d681d7a242ed09bb3837ac9a4cf
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-10 19:58:53 -0800

    Added [Test] to UT in DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -13,7 +13,8 @@ namespace UnitTests.Helpers {
             // Reset
             // Assert             
             Assert.AreEqual(0, result);
-        }   
+        }
+        [Test]
         public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
         {
             // Arrange
@@ -24,6 +25,7 @@ namespace UnitTests.Helpers {
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result >= 6);
         }
+        [Test]
         public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
         {
             // Arrange
@@ -36,6 +38,7 @@ namespace UnitTests.Helpers {
             // Assert             
             Assert.AreEqual(1, result);
         }
+        [Test]
         public void RollDice_Valid_Roll_2_Dice_6_Should_Return_Between_2_And_12()
         {
             // Arrange
@@ -46,6 +49,7 @@ namespace UnitTests.Helpers {
             Assert.AreEqual(true, result >= 2);
             Assert.AreEqual(true, result >= 12);
         }
+        [Test]
         public void RollDice_Invalid_Roll_0_Dice_10_Should_Return_Zero()
         {
             // Arrange
@@ -55,6 +59,7 @@ namespace UnitTests.Helpers {
             // Assert             
             Assert.AreEqual(0, result);
         }
+        [Test]
         public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
         {
             // Arrange
@@ -64,6 +69,7 @@ namespace UnitTests.Helpers {
             // Assert             
             Assert.AreEqual(0, result);
         }
+        [Test]
         public void RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5()
         {
             // Arrange
@@ -76,6 +82,7 @@ namespace UnitTests.Helpers {
             // Assert             
             Assert.AreEqual(5, result);
         }
+        [Test]
         public void RollDice_Valid_Roll_3_Dice_10_Fixed_15_Should_Return_15()
         {
             // Arrange
