3da85c7dea786fcb02feb587a2a4b84b5218e8a2
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-10 19:27:47 -0800

    Added UT for HomeMenuItem to HomeMenuItemTests.cs

---
+++ UnitTests/Models/HomeMenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Mine.Models;
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class HomeMenuItemTests
+    {
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_()
+        {
+            var result = new HomeMenuItem();
+
+            Assert.IsNotNull(result);
+        }
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Game;
+            result.Title = "Text";
+  
+            Assert.AreEqual(MenuItemType.Game, result.Id);
+            Assert.AreEqual("Text", result.Title);
+
+        }
+    }
+}
