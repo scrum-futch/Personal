f0e8ad66c25027531b3b6e37af05b747664d9eef
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2021-02-10 19:21:55 -0800

    Added ItemModel_Get UT to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -15,5 +15,26 @@ namespace UnitTests.Models
 
             Assert.IsNotNull(result);
         }
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_Should_Pass()
+        {
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+
+            Assert.AreEqual("Description",result.Description);
+            Assert.AreEqual("ID",result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1,result.Value);
+        }
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            var result = new ItemModel();
+
+            Assert.AreEqual(0, result.Value);
+        }
     }
 }
